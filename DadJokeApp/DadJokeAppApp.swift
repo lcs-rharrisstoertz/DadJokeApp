dbb3829b9bbd369066370f34fcc0181d20af1f39
Rowan Harris-Stoertz <rharrisstoertz@lcs.on.ca>
2021-02-19 11:13:35 -0500

    Initial Commit

---
+++ DadJokeApp/DadJokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokeAppApp.swift
+//  DadJokeApp
+//
+//  Created by Harris-Stoertz, Rowan on 2021-02-19.
+//
+
+import SwiftUI
+
+@main
+struct DadJokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
